07196b9bfab2add9ab0354723b3518ac562dfd94
Robin Miller <robin.miller39@gmail.com>
2014-02-26 14:43:36 -0800

    functional expense tracker with Category and Purchase objects. Does not have overall spending calculations yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,188 +1,87 @@
-var Contact = {
+var Category = {
   all: [],
-  create: function(firstName, lastName) {
-    var contact = Object.create(Contact);
-    contact.initialize(firstName, lastName);
-    Contact.all.push(contact);
-    return contact;
+  initialize: function(name) {
+    this.name = name;
+    this.purchases = [];
   },
 
-  initialize: function(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.addresses = [];
-    this.phoneNumbers = [];
+  create: function(name) {
+    var category = Object.create(Category);
+    category.initialize(name);
+    Category.all.push(category);
+    return category;
   },
 
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
-  },
-
-  createAddress: function(street, city, state) {
-    var address = Address.create(street, city, state);
-    this.addresses.push(address);
-    return address;
-  },
-
-  createPhoneNumber: function(string) {
-    var phoneNumber = PhoneNumber.create(string);
-    this.phoneNumbers.push(phoneNumber);
-    return phoneNumber;
+  createPurchase: function(amount, description) {
+    var purchase = Purchase.create(amount, description);
+    this.purchases.push(purchase);
+    return purchase;
   }
 };
 
-var Address = {
-  all: [],
-  create: function(street, city, state) {
-    var address = Object.create(Address);
-    address.initialize(street, city, state);
-    Address.all.push(address);
-    return address;
+var Purchase = {
+  create: function(amount, description) {
+    var purchase = Object.create(Purchase);
+    purchase.initialize(amount, description);
+    return purchase;
   },
 
-  initialize: function(street, city, state) {
-    this.street = street;
-    this.city = city;
-    this.state = state;
-  },
-
-  fullAddress: function() {
-    return this.street + ", " + this.city + ", " + this.state;
-  },
-  valid: function() {
-    return !((/[^a-zA-Z]/g).test(this.state) || (/[^a-zA-Z]/g).test(this.city) || (/\D/).test(this.street.charAt(0)) || this.street === "" || this.city === "" || this.state === "" );
+  initialize: function(amount, description) {
+    this.amount = amount;
+    this.description = description;
   }
 };
-
-var PhoneNumber = {
-  all: [],
-  create: function(string) {
-    var phoneNumber = Object.create(PhoneNumber);
-    phoneNumber.initialize(string);
-    PhoneNumber.all.push(phoneNumber);
-    return phoneNumber;
-  },
-
-  initialize: function(string) {
-    this.string = string;
-    this.areaCode = string.substring(0,3);
-    this.firstThree = string.substring(3,6);
-    this.secondFour = string.substring(6,10);
-  },
-
-  fullNumber: function() {
-    return this.areaCode + "-" + this.firstThree + "-" + this.secondFour;
-  },
-  valid: function() {
-    return !((/[^\(\)\-\.\d\s]/g).test(this.string) || this.string.replace((/\D/g), "").length !== 10);
-  }
-}
-
 
 $(document).ready(function() {
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address-append">' + 
-                                 '<div class="form-group">' + 
-                                   '<label for="new-street">Street</label>' + 
-                                   '<input type="text" class="form-control new-street">' + 
-                                 '</div>' + 
-                                 '<div class="form-group">' + 
-                                   '<label for="new-city">City</label>' + 
-                                   '<input type="text" class="form-control new-city">' + 
-                                 '</div>' + 
-                                 '<div class="form-group">' + 
-                                   '<label for="new-state">State</label>' + 
-                                   '<input type="text" class="form-control new-state">' + 
-                                 '</div>' + 
-                               '</div>');
-  });
+  var currentCategory;
 
-  $("#add-phone-number").click(function() {
-    $("#new-phone-numbers").append('<div class="new-phone-number-append">' + 
-                                      '<div class="form-group">' + 
-                                        '<label for="new-phone-number">Phone number</label>' + 
-                                        '<input type="text" class="form-control new-phone-number">' + 
-                                      '</div>' +                                 
-                                    '</div>');
-  });
-
-  $("form#new-contact").submit(function(event) {
+  $("form#new-category").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var newContact = Contact.create(inputtedFirstName, inputtedLastName);
+    inputtedCategory = $("input#new-category-name").val();
 
-    var inputtedStreet = $("div.new-address input.new-street").val();
-    var inputtedCity = $("div.new-address input.new-city").val();
-    var inputtedState = $("div.new-address input.new-state").val();
-    var newAddress = newContact.createAddress(inputtedStreet, inputtedCity, inputtedState);
+    newCategory = Category.create(inputtedCategory);
 
-    var inputtedPhoneNumber = $("input#new-phone-number").val();
-    var inputtedPhoneNumberScrubbed = inputtedPhoneNumber.replace(/\D/g, "");
-    var newPhoneNumber = newContact.createPhoneNumber(inputtedPhoneNumberScrubbed);
+    $("ul#all-category-list").append("<li class='category-clickable'>" + newCategory.name + "</li>");
 
-    $(".new-phone-number-append").each(function() {
-      var inputtedPhoneNumber = $(this).find("input.new-phone-number").val();
-      var inputtedPhoneNumberScrubbed = inputtedPhoneNumber.replace(/\D/g, "");
-      var newPhoneNumber = newContact.createPhoneNumber(inputtedPhoneNumberScrubbed);
-    });
+    $(".appended-row").remove();
 
+    $("#add-purchase-section").show();
+    $("#show-purchases-section").show();
 
-    $(".new-address-append").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = newContact.createAddress(inputtedStreet, inputtedCity, inputtedState);
-    });
+    $(".category-clickable").last().click(function() {   
+      currentCategory = Category.all[$(this).index()];
 
-    var badPhoneNumberExists;
-    newContact.phoneNumbers.forEach(function(phoneNumber) {
-      if (!phoneNumber.valid()) {
-        badPhoneNumberExists = true;
+      $(".appended-row").remove();
+      if (currentCategory.purchases.length > 0) {
+        currentCategory.purchases.forEach(function(purchase) {
+        $("#purchases-table").append("<tr class='appended-row'><td>" + purchase.description + "</td><td>" + purchase.amount + "</td></tr>");
+        });
       }
+
     });
-
-    var badAddressExists;
-        newContact.addresses.forEach(function(address) {
-          if (!address.valid()) {
-            badAddressExists = true;
-          }
-        });
-
-
-    if (badPhoneNumberExists || badAddressExists) {
-      if (badPhoneNumberExists) {
-        alert("Please enter a valid phone number.")
-      } 
-      if (badAddressExists) {
-      alert("Please enter a valid address!")
-      } 
-    } else {
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-    
-      $(".contact").last().click(function() {
-        $("#show-contact").show();
-
-        $("#show-contact h2").text(newContact.fullName());
-        $(".first-name").text(newContact.firstName);
-        $(".last-name").text(newContact.lastName);
-
-        $("ul#addresses").text("");
-        newContact.addresses.forEach(function(address) {
-          $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-        });
-        $("ul#phoneNumbers").text("");
-        newContact.phoneNumbers.forEach(function(phoneNumber) {
-          $("ul#phoneNumbers").append("<li>" + phoneNumber.fullNumber() + "</li>");
-        });
-      });
-
-      $(".new-address-append").remove();
-      $(".new-phone-number-append").remove();
-
-      this.reset();
-    }
+    currentCategory = newCategory;
+    this.reset();
   });
-});
 
+  $("form#new-purchase").submit(function(event) {
+    event.preventDefault();
+
+    inputtedDescription = $("input#new-purchase-description").val();
+    inputtedAmount = parseFloat($("input#new-purchase-amount").val()).toFixed(2);
+
+    newPurchase = currentCategory.createPurchase(inputtedAmount, inputtedDescription);
+
+    $(".appended-row").remove();
+    currentCategory.purchases.forEach(function(purchase) {
+        $("#purchases-table").append("<tr class='appended-row'><td>" + purchase.description + "</td><td>" + purchase.amount + "</td></tr>");
+    });
+    this.reset();
+  });
+
+  // $("#append-table-rows").text("");
+  // currentCategory.purchases.forEach(function(purchase) {
+  //   $("#append-table-rows").append("<tr><td>" + purchase.description + "</td><td>" + purchase.amount + "</td></tr>");
+  // });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,217 +1,79 @@
 beforeEach(function() {
-  Contact.all = [];
-  Address.all = [];
-  PhoneNumber.all = [];
-});
+  Category.all = [];
+})
 
-describe('Contact', function() {
-  describe('fullName', function() {
-    it("returns the full name with a space in between", function() {
-      var testContact = Object.create(Contact);
-      testContact.firstName = "Robin";
-      testContact.lastName = "Miller";
-      testContact.fullName().should.equal("Robin Miller")
-    });
-  });
-
+describe('Purchase', function(){
   describe('initialize', function() {
-    it('sets the first and last name for a contact', function() {
-      var testContact = Object.create(Contact);
-      testContact.initialize("Robin", "Miller");
-      testContact.firstName.should.equal("Robin");
-      testContact.lastName.should.equal("Miller");
-    });
-
-    it('creates the empty addresses array', function() {
-      var testContact = Object.create(Contact);
-      testContact.initialize();
-      testContact.addresses.should.eql([]);
-    });
-
-    it('creates the empty phone numbers array', function() {
-      var testContact = Object.create(Contact);
-      testContact.initialize();
-      testContact.phoneNumbers.should.eql([]);
+    it('should set the amount and description properties', function() {
+      var testPurchase = Object.create(Purchase);
+      testPurchase.initialize(5.99, "cold medicine");
+      testPurchase.amount.should.equal(5.99);
+      testPurchase.description.should.equal("cold medicine");
     });
   });
 
   describe('create', function() {
-    it('creates a new instance of object type Contact', function() {
-      var testContact = Contact.create();
-      Contact.isPrototypeOf(testContact).should.equal(true);
+    it('creates a new purchase object', function() {
+      var testPurchase = Purchase.create();
+      Purchase.isPrototypeOf(testPurchase).should.equal(true);
     });
 
-    it('initializes the contact', function() {
-      var testContact = Contact.create();
-      testContact.addresses.should.eql([]);
-    });
-
-    it('should add the .all property to the contact', function() {
-      var testContact = Contact.create();
-      Contact.all.should.eql([testContact]);
-    });
-  });
-
-  describe('createAddress', function() {
-    it('should create an address object', function() {
-      var testContact = Contact.create();
-      var testAddress = testContact.createAddress();
-      Address.isPrototypeOf(testAddress).should.equal(true);
-    });
-
-    it('should initialze the address object', function() {
-      var testContact = Contact.create();
-      var testAddress = testContact.createAddress("123 Main", "Portland", "OR");
-      testAddress.street.should.equal("123 Main");
-    })
-
-    it('should add the created address to the address property of the contact', function() {
-      var testContact = Contact.create();
-      var testAddress = testContact.createAddress();
-      testContact.addresses.should.eql([testAddress]);
-    });
-  });
-
-  describe('createPhoneNumber', function() {
-    it('should create a phoneNumber object', function() {
-      var testContact = Contact.create();
-      var testPhoneNumber = testContact.createPhoneNumber("5417606978");
-      PhoneNumber.isPrototypeOf(testPhoneNumber).should.equal(true);
-    });
-
-    it('initializes phone number object', function() {
-      var testContact = Contact.create();
-      var testPhoneNumber = testContact.createPhoneNumber("5417606978");
-      testPhoneNumber.areaCode.should.equal("541");
-    });
-
-    it('adds the phone number to the phone number property of the contact', function() {
-      var testContact = Contact.create();
-      var testPhoneNumber = testContact.createPhoneNumber("5417606978");
-      testContact.phoneNumbers.should.eql([testPhoneNumber]);
-    });
-  }); 
-});
-
-
-describe('Address', function() {
-  describe('initialize', function() {
-    it('sets the street city and state properties', function() {
-      var testAddress = Object.create(Address);
-      testAddress.initialize("123 4th Ave", "Portland", "OR");
-      testAddress.street.should.equal("123 4th Ave");
-      testAddress.city.should.equal("Portland");
-      testAddress.state.should.equal("OR");
-    });
-  });
-
-  describe('create', function() {
-    it('creates a new address object', function() {
-      var testAddress = Address.create();
-      Address.isPrototypeOf(testAddress).should.equal(true);
-    });
-
-    it('should initialize the address', function() {
-      var testAddress = Address.create("123 street", "City", "ST");
-      testAddress.street.should.equal("123 street");
-    });
-
-    it('should add the address to the .all property', function() {
-      var testAddress = Address.create();
-      Address.all.should.eql([testAddress]);
-    });
-  });
-
-  describe('fullAddress', function() {
-    it("returns the full address with nice formatting", function() {
-      var testAddress = Object.create(Address);
-      testAddress.street = "123 4th Ave";
-      testAddress.city = "Portland";
-      testAddress.state = "Oregon";
-      testAddress.fullAddress().should.equal("123 4th Ave, Portland, Oregon");
-    });
-  });
-
-  describe('valid', function() {
-    it("should return false if the state field contains anything but letters", function() {
-      var testAddress = Object.create(Address);
-      testAddress.state = "Or3gon";
-      testAddress.city = "Portland";
-      testAddress.street = "1234 main st";
-      testAddress.valid().should.equal(false);
-    });
-    it("should return false if the city field contains anything but letters", function() {
-      var testAddress = Object.create(Address);
-      testAddress.state = "Oregon";
-      testAddress.city = "P0rtland";
-      testAddress.street = "1234 main st";
-      testAddress.valid().should.equal(false);
-    });
-    it("should return false if the first character of the street field is not a number", function() {
-      var testAddress = Object.create(Address);
-      testAddress.state = "Oregon";
-      testAddress.city = "Portland";
-      testAddress.street = "I234 main st";
-      testAddress.valid().should.equal(false);
+    it('initializes the new purchase object', function() {
+      var testPurchase = Purchase.create(5.99, "cold medicine");
+      testPurchase.amount.should.equal(5.99);
+      testPurchase.description.should.equal("cold medicine");
     });
   });
 });
 
-describe('PhoneNumber', function() {
-  describe('create', function() {
-    it('should create a new PhoneNumber object', function() {
-      var testPhoneNumber = PhoneNumber.create("5417606978");
-      PhoneNumber.isPrototypeOf(testPhoneNumber).should.equal(true);
-    });
-
-    it('should initialize the PhoneNumber object', function() {
-      var testPhoneNumber = PhoneNumber.create("5417606978");
-      testPhoneNumber.string.should.equal("5417606978");
-    });
-
-    it('should add the object to the .add property', function() {
-      var testPhoneNumber = PhoneNumber.create("5417606978");
-      PhoneNumber.all.should.eql([testPhoneNumber]);
-    });
-  });
-
+describe('Category', function() {
   describe('initialize', function() {
-    it('sets the string, area-code, exchange, and subscriber number', function() {
-      var testPhoneNumber = Object.create(PhoneNumber);
-      testPhoneNumber.initialize("5417606978");
-      testPhoneNumber.string.should.equal("5417606978");
-      testPhoneNumber.areaCode.should.equal("541");
-      testPhoneNumber.firstThree.should.equal("760");
-      testPhoneNumber.secondFour.should.equal("6978");
+    it('sets the name of the category', function() {
+      var testCategory = Object.create(Category);
+      testCategory.initialize("Food");
+      testCategory.name.should.equal("Food");
+    });
+
+    it('creates the empty purchases array', function() {
+      var testCategory = Object.create(Category);
+      testCategory.initialize("Food");
+      testCategory.purchases.should.eql([]);
     });
   });
 
-  describe('fullNumber', function() {
-    it("returns the three parts formatted with dashes in between", function() {
-      var testPhoneNumber = Object.create(PhoneNumber);
-      testPhoneNumber.areaCode = "503";
-      testPhoneNumber.firstThree = "123";
-      testPhoneNumber.secondFour = "1234";
-      testPhoneNumber.fullNumber().should.equal("503-123-1234")
+  describe('create', function() {
+    it('should create a category object', function() {
+      var testCategory = Category.create();
+      Category.isPrototypeOf(testCategory).should.equal(true);
+    });
+    it('should initialize the category object', function() {
+      var testCategory = Category.create();
+      testCategory.purchases.should.eql([]);
+    });
+    it('adds the category to the .add property', function() {
+      var testCategory = Category.create();
+      Category.all.should.eql([testCategory]);
     });
   });
-  describe('valid', function() {
-    it("return false if the input contains any non digit characters", function() {
-      var testPhoneNumber = Object.create(PhoneNumber);
-      testPhoneNumber.string = "503thisisbadstuff1231234";
-      testPhoneNumber.areaCode = "503thisisbadstuff";
-      testPhoneNumber.firstThree = "123";
-      testPhoneNumber.secondFour = "1234";
-      testPhoneNumber.valid().should.equal(false);
+
+  describe('createPurchase', function() {
+    it('creates a purchase object', function() {
+      var testCategory = Category.create();
+      var testPurchase = testCategory.createPurchase();
+      Purchase.isPrototypeOf(testPurchase).should.equal(true);
     });
 
-    it("return true if the input contains only numbers", function() {
-      var testPhoneNumber = Object.create(PhoneNumber);
-      testPhoneNumber.string = "(503) 123-1234";
-      testPhoneNumber.areaCode = "503";
-      testPhoneNumber.firstThree = "123";
-      testPhoneNumber.secondFour = "1234";
-      testPhoneNumber.valid().should.equal(true);
+    it('initializes the purchase object', function() {
+      var testCategory = Category.create();
+      var testPurchase = testCategory.createPurchase(1.99, "bread");
+      testPurchase.amount.should.equal(1.99);
+      testPurchase.description.should.equal("bread");
+    });
+
+    it('pushes the purchase object to the purcheses array of the Category', function() {
+      var testCategory = Category.create();
+      var testPurchase = testCategory.createPurchase();
+      testCategory.purchases.should.eql([testPurchase]);
     });
   });
 });
